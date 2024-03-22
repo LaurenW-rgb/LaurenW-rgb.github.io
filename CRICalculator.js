89632fc0df3e0023e6f3f4ae941d6055801b362a
laurenwillis <laurentheflyingllama@gmail.com>
2024-03-22 12:32:57 +0000

    concentration micrograms and milligrams options

--- CRICalculator.js
+++ CRICalculator.js
@@ -2,6 +2,16 @@ function calculateCRIRate() {
     var bodyWeight = document.getElementById('bodyWeight').value;
     var CRIRate = document.getElementById('CRIRate').value;
     var concentration = document.getElementById('concentration').value;
-    var result = (bodyWeight * CRIRate)/concentration
+
+    var doseUnit = document.getElementById('doseUnit').value;
+    var doseDuration = document.getElementById('doseDuration').value;
+    var concentrationUnit = document.getElementById('concentrationUnit').value;
+
+    if (concentrationUnit === "milligramsConc"){
+        var result = (bodyWeight * CRIRate)/concentration;
+    } else if (concentrationUnit === "microgramsConc"){
+        var result = (bodyWeight * CRIRate)/(concentration/1000);
+    };
+
     document.getElementById("result").textContent= "ml/h = " + result.toFixed(1);
 }
