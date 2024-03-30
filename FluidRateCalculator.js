78347de99716abfd0e4e3ec80cda07346f7fff36
laurenwillis <laurentheflyingllama@gmail.com>
2024-03-30 15:29:26 +0000

    renaming

--- FluidRateCalculator.js
+++ FluidRateCalculator.js
@@ -1,7 +1,7 @@
 function calculateFluidRate() {
     var bodyWeight = document.getElementById('bodyWeight').value;
-    var fluidrate = document.getElementById('fluidRate').value;
-    var dropsperml = document.getElementById('dropsPerMl').value;
-    var result = (bodyWeight * fluidrate * dropsperml)/86400
-    document.getElementById("result").textContent= "Drops per second = " + result.toFixed(2);
+    var fluidRate = document.getElementById('fluidRate').value;
+    var dropsPerMl = document.getElementById('dropsPerMl').value;
+    var dropsPerSecond = (bodyWeight * fluidRate * dropsPerMl)/86400
+    document.getElementById("dropsPerSecond").textContent= "Drops per second = " + dropsPerSecond.toFixed(2);
 }
