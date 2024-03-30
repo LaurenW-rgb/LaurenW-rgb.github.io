a2ed41f7bc918cfa916b1d6b049036c1501ab201
laurenwillis <laurentheflyingllama@gmail.com>
2024-03-30 15:31:22 +0000

    add litres per hour

--- FluidRateCalculator.js
+++ FluidRateCalculator.js
@@ -2,6 +2,8 @@ function calculateFluidRate() {
     var bodyWeight = document.getElementById('bodyWeight').value;
     var fluidRate = document.getElementById('fluidRate').value;
     var dropsPerMl = document.getElementById('dropsPerMl').value;
-    var dropsPerSecond = (bodyWeight * fluidRate * dropsPerMl)/86400
+    var litresPerHour = (bodyWeight * fluidRate)/24000;
+    var dropsPerSecond = (bodyWeight * fluidRate * dropsPerMl)/86400;
+    document.getElementById("litresPerHour").textContent= "Litres per hour = " + litresPerHour.toFixed(2);
     document.getElementById("dropsPerSecond").textContent= "Drops per second = " + dropsPerSecond.toFixed(2);
 }
