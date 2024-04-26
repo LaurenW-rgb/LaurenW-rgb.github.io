b132df721d7d111d83c1d623813e9a3c1d091a13
laurenwillis <laurentheflyingllama@gmail.com>
2024-04-26 15:11:08 +0100

    added option of days or weeks to prescription calculator, though is some weird error happening in calculation

--- PrescriptionCalculator.js
+++ PrescriptionCalculator.js
@@ -1,11 +1,42 @@
+function getFirstAmountInDays (firstAmount, firstDurationUnit) {
+    if (firstDurationUnit === "durationDays"){
+        return firstAmount;
+    }
+    else if (firstDurationUnit === "durationWeeks"){
+        return firstAmount*7;
+    };
+};
+
+function getSecondAmountInDays (secondAmount, secondDurationUnit) {
+    if (secondDurationUnit === "durationDays"){
+        return secondAmount;
+    }
+else if (secondDurationUnit === "durationWeeks"){
+        return secondAmount*7;
+    };
+};
+
+function getThirdAmountInDays (thirdAmount, thirdDurationUnit) {
+    if (thirdDurationUnit === "durationDays"){
+        return thirdAmount;
+    }
+else if (thirdDurationUnit === "durationWeeks"){
+        return thirdAmount*7;
+    };
+};
+
 function calculatePrescription(){
     var firstAmount = document.getElementById('firstAmount').value;
-    var firstDuration = document.getElementById('firstDuration').value;
+    var firstDurationUnit = document.getElementById('firstDurationUnit').value;
     var secondAmount = document.getElementById('secondAmount').value;
-    var secondDuration = document.getElementById('secondDuration').value;
+    var secondDurationUnit = document.getElementById('secondDurationUnit').value;
     var thirdAmount = document.getElementById('thirdAmount').value;
-    var thirdDuration = document.getElementById('thirdDuration').value;
+    var thirdDurationUnit = document.getElementById('thirdDurationUnit').value;
 
-    var totalPrescription = (firstAmount * firstDuration * 7) + (secondAmount * secondDuration * 7) + (thirdAmount * thirdDuration * 7);
+    var firstAmountInDays = getFirstAmountInDays(firstAmount, firstDurationUnit);
+    var secondAmountInDays = getSecondAmountInDays(secondAmount, secondDurationUnit);
+    var thirdAmountInDays = getThirdAmountInDays(thirdAmount, thirdDurationUnit);
+
+    var totalPrescription = firstAmountInDays + secondAmountInDays + thirdAmountInDays;
     document.getElementById("totalPrescription").textContent= "Total prescription = " + totalPrescription + " sachets/mls";
 };
