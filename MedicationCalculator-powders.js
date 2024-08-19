077b47392ecbed5b9863c117c222b57ecb4cac77
laurenwillis <laurentheflyingllama@gmail.com>
2024-08-19 18:22:44 +0100

    added presets to powder medication calculator, with values for equisolon and sucralfate

--- MedicationCalculator-powders.js
+++ MedicationCalculator-powders.js
@@ -1,4 +1,22 @@
 "use strict";
+function myFunction() {
+    insertPreset();
+    calculatePowderMedication();
+}
+
+function insertPreset() {
+    var drugPreset = document.getElementById("drug").value;
+    if ((drugPreset = "sucralfate")) {
+        document.getElementById("dose").value = 20;
+        document.getElementById("concentration").value = 1000;
+        document.getElementById("gramsPerUnit").value = 3;
+    }
+    if ((drugPreset = "equisolon")) {
+        document.getElementById("dose").value = 1.1;
+        document.getElementById("concentration").value = 33;
+        document.getElementById("gramsPerUnit").value = 4.5;
+    }
+}
 
 function calculatePowderMedication() {
     var bodyWeight = document.getElementById("bodyWeight").value;
@@ -10,18 +28,17 @@ function calculatePowderMedication() {
     var scoopsOrSachets = totalGrams / gramsPerUnit;
 
     if (isNaN(totalGrams)) {
-        document.getElementById("totalGrams").textContent = "0.0";
+        document.getElementById("totalGrams").textContent = "";
     } else if (!isFinite(totalGrams)) {
-        document.getElementById("totalGrams").textContent = "0.0";
+        document.getElementById("totalGrams").textContent = "";
     } else {
         document.getElementById("totalGrams").textContent =
             totalGrams.toFixed(1);
     }
-
     if (isNaN(scoopsOrSachets)) {
-        document.getElementById("scoopsOrSachets").textContent = "0.0";
+        document.getElementById("scoopsOrSachets").textContent = "";
     } else if (!isFinite(scoopsOrSachets)) {
-        document.getElementById("scoopsOrSachets").textContent = "0.0";
+        document.getElementById("scoopsOrSachets").textContent = "";
     } else {
         document.getElementById("scoopsOrSachets").textContent =
             scoopsOrSachets.toFixed(1);
