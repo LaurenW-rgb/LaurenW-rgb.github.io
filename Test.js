0f1c320d0b14c3470f7d9639562986f53306a276
laurenwillis <laurentheflyingllama@gmail.com>
2024-10-04 12:41:36 +0100

    dose and concentration unit select on test

--- Test.js
+++ Test.js
@@ -1,12 +1,36 @@
 "use strict";
+function getConcentrationInMilligrams(rawConcentration, concentrationUnit) {
+    if (concentrationUnit === "milligramsConc") {
+        return rawConcentration * 1;
+    } else if (concentrationUnit === "microgramsConc") {
+        return rawConcentration / 1000;
+    }
+}
+
+function getDoseUnitInMilligrams(rawDose, doseUnit) {
+    if (doseUnit === "milligramsDose") {
+        return rawDose * 1;
+    } else if (doseUnit === "microgramsDose") {
+        return rawDose / 1000;
+    }
+}
 
 function calculateMedication(e) {
     e.preventDefault();
     var bodyweight = document.getElementById("bodyweight").value;
-    var concentration = document.getElementById("concentration").value;
-    var dose = document.getElementById("dose").value;
 
-    var result = (bodyweight * dose) / concentration;
+    var rawConcentration = document.getElementById("concentration").value;
+    var concentrationUnit = document.getElementById("concentrationUnit").value;
+    var concentrationMilligrams = getConcentrationInMilligrams(
+        rawConcentration,
+        concentrationUnit,
+    );
+
+    var rawDose = document.getElementById("dose").value;
+    var doseUnit = document.getElementById("doseUnit").value;
+    var doseMilligrams = getDoseUnitInMilligrams(rawDose, doseUnit);
+
+    var result = (bodyweight * doseMilligrams) / concentrationMilligrams;
     if (isNaN(result)) {
         document.getElementById("result").textContent = "Error";
     } else if (!isFinite(result)) {
