150fc6341d64a377847366df2ab9187daba2128f
laurenwillis <laurentheflyingllama@gmail.com>
2026-07-03 22:42:53 +0100

    local database set up

--- CarInventory.js
+++ CarInventory.js
@@ -1,5 +1,18 @@
 "use strict";
 
+class LocalDatabase {
+    getStock(drug) {
+        const stock = Number.parseInt(localStorage.getItem(drug));
+        return stock || 0;
+    }
+
+    setStock(drug, amount) {
+        localStorage.setItem(drug, amount)
+    }
+}
+
+const db = new LocalDatabase();
+
 function calculateInventory(e) {
     e.preventDefault();
     var drug = document.getElementById("drug").value;
@@ -9,4 +22,6 @@ function calculateInventory(e) {
     console.log(drug)
     console.log(direction)
     console.log(amount)
+
+    db.setStock(drug, amount)
 }
