849d7a83ccbe3d780af04f021d5b0e2bccbcc3f1
laurenwillis <laurentheflyingllama@gmail.com>
2026-07-03 22:29:29 +0100

    gets value of ml now

--- CarInventory.js
+++ CarInventory.js
@@ -4,7 +4,7 @@ function calculateInventory(e) {
     e.preventDefault();
     var drug = document.getElementById("drug").value;
     var direction = document.getElementById("direction").value;
-    var amount = document.getElementById("amount");
+    var amount = document.getElementById("amount").value;
 
     console.log(drug)
     console.log(direction)
