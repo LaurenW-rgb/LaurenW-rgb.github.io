c133cd37f87f6520407f0a23e26af20b6b2d247f
laurenwillis <laurentheflyingllama@gmail.com>
2026-07-03 22:28:58 +0100

    set up js to get values from html

---
+++ CarInventory.js
@@ -0,0 +1,12 @@
+"use strict";
+
+function calculateInventory(e) {
+    e.preventDefault();
+    var drug = document.getElementById("drug").value;
+    var direction = document.getElementById("direction").value;
+    var amount = document.getElementById("amount");
+
+    console.log(drug)
+    console.log(direction)
+    console.log(amount)
+}
