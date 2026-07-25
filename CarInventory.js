7d0ccccb5d55cbf81238ac397525d977117afca6
laurenwillis <laurentheflyingllama@gmail.com>
2026-07-25 22:00:41 +0100

    databse working for flunixin and adding/using/message streamlined

--- CarInventory.js
+++ CarInventory.js
@@ -1,19 +1,24 @@
 "use strict";
 
-// class LocalDatabase {
-//     getStock(drug) {
-//         const stock = Number.parseFloat(localStorage.getItem(drug));
-//         return stock || 0;
-//     }
-//
-//     setStock(drug, amount) {
-//         localStorage.setItem(drug, amount)
-//     }
-// }
-//
-// const db = new LocalDatabase();
+class LocalDatabase {
+    getStock(drug) {
+        const stock = Number.parseFloat(localStorage.getItem(drug));
+        return stock || 0;
+    }
 
-var flunixinAmount = 0;
+    setStock(drug, amount) {
+        localStorage.setItem(drug, amount)
+    }
+
+    changeStock(drug, delta) {
+        var amount = this.getStock(drug);
+        amount += delta;
+        this.setStock(drug, amount);
+    }
+}
+
+const db = new LocalDatabase();
+
 var medesedanAmount = 0;
 var butadorAmount =0;
 
@@ -21,33 +26,29 @@ function calculateInventory(e) {
     e.preventDefault();
     var drug = document.getElementById("drug").value;
     var direction = document.getElementById("direction").value;
-    var amount = Number.parseFloat(document.getElementById("amount").value);
+    var delta = Number.parseFloat(document.getElementById("amount").value);
     var newStock = document.getElementById("newStock").textContent;
 
     console.log(drug)
     console.log(direction)
-    console.log(amount)
+    console.log(delta)
 
     if (direction === "used") {
-        amount *= -1;
+        delta *= -1;
     }
-
-    if (drug === "flunixin") {
-        flunixinAmount += amount;
-        console.log(flunixinAmount);
-        document.getElementById("newStock").textContent = "There is " + flunixinAmount + "ml flunixin in the car"
-    }
-    ;
+    
+        db.changeStock(drug, delta)
+        document.getElementById("newStock").textContent = "There is " + db.getStock(drug) + "ml " + drug + " in the car"
 
     if (drug === "medesedan") {
-        medesedanAmount += amount;
+        medesedanAmount += delta;
         console.log(medesedanAmount);
         document.getElementById("newStock").textContent = "There is " + medesedanAmount + "ml medesedan in the car"
     }
     ;
 
     if (drug === "butador") {
-        butadorAmount += amount;
+        butadorAmount += delta;
         console.log(butadorAmount);
         document.getElementById("newStock").textContent = "There is " + butadorAmount + "ml butador in the car"
     };
