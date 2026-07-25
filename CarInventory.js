c4fbf1c5267165917ac722bd2552da41900e6a56
laurenwillis <laurentheflyingllama@gmail.com>
2026-07-25 21:19:25 +0100

    vols add for flunixin

--- CarInventory.js
+++ CarInventory.js
@@ -1,27 +1,32 @@
 "use strict";
 
-class LocalDatabase {
-    getStock(drug) {
-        const stock = Number.parseInt(localStorage.getItem(drug));
-        return stock || 0;
-    }
+// class LocalDatabase {
+//     getStock(drug) {
+//         const stock = Number.parseInt(localStorage.getItem(drug));
+//         return stock || 0;
+//     }
+//
+//     setStock(drug, amount) {
+//         localStorage.setItem(drug, amount)
+//     }
+// }
 
-    setStock(drug, amount) {
-        localStorage.setItem(drug, amount)
-    }
-}
+// const db = new LocalDatabase();
 
-const db = new LocalDatabase();
+var flunixinAmount = 0;
 
 function calculateInventory(e) {
     e.preventDefault();
     var drug = document.getElementById("drug").value;
     var direction = document.getElementById("direction").value;
-    var amount = document.getElementById("amount").value;
+    var amount = Number.parseFloat(document.getElementById("amount").value);
 
     console.log(drug)
     console.log(direction)
     console.log(amount)
 
-    db.setStock(drug, amount)
+    flunixinAmount += amount;
+
+    console.log(flunixinAmount);
+
 }
